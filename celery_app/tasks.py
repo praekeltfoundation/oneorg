c4148cf4d107ac3ccb23df139f97c38542a64ccc
Mike Jones <mike@westerncapelabs.com>
2014-03-21 09:38:50 +0200

    Update and clean Mxit import tests

--- celery_app/tasks.py
+++ celery_app/tasks.py
@@ -1,17 +1,23 @@
 from celery import task
 import csv
 from metrics_manager.models import IncomingData
-from datetime import datetime
+from dateutil import parser
 
 
 @task()
 def ingest_csv(csv_data, channel):
+    headers = {
+        "mxit": ["Date", "UserID", "Nick", "Mxit Email", "Name & Surname", "Mobile",
+            "Optional. email address - (Dont have an email address? Use your mxit address (mxitid@mxit.im)",
+            "Country"]
+    }
     records = csv.reader(csv_data, delimiter=',', quotechar='"')
     if channel.name == "mxit":
         for line in records:
-            if len(line) == 7 and line[0] != "Date": 
+            if len(line) > 6 and line != headers["mxit"]: 
                 incoming_data = IncomingData()
-                incoming_data.source_timestamp = datetime.strptime(line[0], "%Y-%m-%d %H:%M:%S")
+                incoming_data.source_timestamp = parser.parse(line[0] + " UTC")
                 incoming_data.channel = channel
                 incoming_data.channel_uid = line[1]
+                incoming_data.email = line[3]
                 incoming_data.save()
--- celery_app/tests.py
+++ celery_app/tests.py
@@ -2,9 +2,23 @@
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from StringIO import StringIO
 
+from celery_app.tasks import ingest_csv
+from metrics_manager.models import Channel, IncomingData
 
 class TestUploadCSV(TestCase):
+
+    M_SEP = ("sep=,\r\n")
+    M_HEADER = ("Date,UserID,Nick,\"Mxit Email\",\"Name & Surname\",Mobile,"
+                "\"Optional. email address - (Dont have an email address? "
+                "Use your mxit address (mxitid@mxit.im)\",Country\r\n")
+    M_LINE_CLEAN_1 = ("\"2014-02-02 15:34:13\",m00000000001,User1,user1@mxit.im,"
+                    "Joyclere,\"Hy guyz\",0700000001\r\n")
+    M_LINE_CLEAN_2 = ("\"2014-02-04 12:35:34\",m00000000002,User2,user2@mxit.im,"
+                    "\"Malefa tshabalala\",0700000002,user2@mxit.com\r\n")
+
+    fixtures = ["channel.json"]
     def setUp(self):
         self.admin = User.objects.create_superuser('test', 'test@example.com', "pass123")
 
@@ -17,3 +31,11 @@ class TestUploadCSV(TestCase):
 
         response = self.client.post(reverse("csv_uploader"))
         self.assertIn("Upload CSV", response.content)
+
+    def test_upload_mxit_clean(self):
+        channel = Channel.objects.get(name="mxit")
+        clean_sample =  self.M_SEP + self.M_HEADER + self.M_LINE_CLEAN_1 + self.M_LINE_CLEAN_2
+        uploaded = StringIO(clean_sample)
+        ingest_csv(uploaded, channel)
+        imported = IncomingData.objects.get(channel_uid="m00000000002")
+        self.assertEquals(imported.email, "user2@mxit.im")
